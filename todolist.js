2f5fc4f86f004b8304bb41254f6ad1f6e365e919
harshith-m-r <harshith.hr436@gmail.com>
2022-08-19 17:29:36 +0530

    update: changed conditions for if-else

--- todolist.js
+++ todolist.js
@@ -10,31 +10,31 @@
   let taskName = '';
 
   function createTask() {
-    if (domElems.inputField.value === '') {
+    if (domElems.inputField.value) {
+      const paragraph = document.createElement('p');
+      paragraph.innerText = domElems.inputField.value;
+      taskName = domElems.inputField.value;
+
+      taskObj[Date.now()] = {
+        id: Date.now(),
+        taskName: taskName,
+        isComplete: false,
+      };
+      localStorage.setItem('Tasks', JSON.stringify(taskObj));
+      // localStorage.setItem('ids', JSON.stringify(idArray));
+      // console.log(taskArray);
+
+      paragraph.addEventListener('click', function () {
+        paragraph.style.textDecoration = 'line-through';
+      });
+
+      domElems.toDoContainer.appendChild(paragraph);
+      domElems.inputField.value = '';
+      // i += 1;
+    } else {
       alert('please enter something.!');
       return;
     }
-    const paragraph = document.createElement('p');
-    paragraph.innerText = domElems.inputField.value;
-    taskName = domElems.inputField.value;
-
-    taskObj[Date.now()] = {
-      id: Date.now(),
-      taskName: taskName,
-      isComplete: false,
-    };
-
-    localStorage.setItem('Tasks', JSON.stringify(taskObj));
-    // localStorage.setItem('ids', JSON.stringify(idArray));
-    // console.log(taskArray);
-
-    paragraph.addEventListener('click', function () {
-      paragraph.style.textDecoration = 'line-through';
-    });
-
-    domElems.toDoContainer.appendChild(paragraph);
-    domElems.inputField.value = '';
-    // i += 1;
   }
 
   domElems.addToDoButton.addEventListener('click', createTask);
@@ -54,17 +54,7 @@
   dispBtn.addEventListener('click', getDataFromLocalStorage);
 
   function displayTasks(taskData) {
-    if (!taskData) {
-      alert('The local storage is empty');
-      return;
-    } else {
-      // taskKeys.forEach((key) => {
-      // let taskDisplay = document.querySelector('.dispTask');
-      // dispTask = document.createElement('div');
-      // dispTask.innerText = taskData[key].taskName;
-      // taskDisplay.append(dispTask);
-      // });
-
+    if (taskData) {
       for (const key in taskData) {
         if (taskData.hasOwnProperty.call(taskData, key)) {
           // const element = taskData[key];
@@ -75,6 +65,15 @@
           taskDisplay.append(dispTask);
         }
       }
+    } else {
+      // taskKeys.forEach((key) => {
+      // let taskDisplay = document.querySelector('.dispTask');
+      // dispTask = document.createElement('div');
+      // dispTask.innerText = taskData[key].taskName;
+      // taskDisplay.append(dispTask);
+      // });
+      alert('The local storage is empty');
+      return;
     }
   }
 })();
